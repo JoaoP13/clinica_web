80f9b028879bb6fadc5eef9f67ea441700058571
Lucas Alexsanderson da Fonseca <lucas.alexsanderson@blip.ai>
2023-12-13 18:36:52 -0300

    add router clinica

--- back-end/src/routes/index.routes.ts
+++ back-end/src/routes/index.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import pessoaRouter from "./pessoa.routes";
 import pacienteRouter from "./paciente.routes";
 import enderecoRouter from "./endereco.routes";
+import clinicaRouter  from "./clinica.routes";
 
 const router: Router = Router();
 
@@ -10,5 +11,6 @@ router.use('/pessoa', pessoaRouter);
 router.use('/paciente', pacienteRouter);
 router.use('/endereco', enderecoRouter);
 router.use('/especialidade', enderecoRouter);
+router.use('/clinica', clinicaRouter);
 
 export default router;
